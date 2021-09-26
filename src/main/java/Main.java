cffb3c98e8ae2074695c2e7283b6792e326b075a
Валентин <valmak04@gmail.com>
2021-09-26 23:06:21 +0400

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Scanner sc = new Scanner(System.in);
+        int n = 0, i = 0, ans = 0, n1 = 0;
+
+        do {
+            n1 = sc.nextInt();
+            n += n1;
+            if (n1 == 0) break;
+            i += 1;
+            if (n == 10 & ans == 0) ans = i;
+        }while (n != 0);
+
+        System.out.println("ANS:"+ans);
     }
 }
